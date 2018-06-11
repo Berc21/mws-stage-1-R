f253592820b882b14cead48da974892fb2425640
Berkan Cetinkaya <berkan.cetinkaya@windowslive.com>
2018-06-11 18:42:10 +0300

    add constructor and fetch method to fetchhelper class

--- js/fetchhelper.js
+++ js/fetchhelper.js
@@ -1,6 +1,26 @@
 
 class FetchHelper {
 
+  constructor(url) {
+    this.url = url;
+  }
+
+  fetch(callback) {
+    let xhr = new XMLHttpRequest();
+    xhr.open('GET', this.url);
+    xhr.onload = () => {
+      if (xhr.status === 200) { // Got a success response from server!
+        const json = JSON.parse(xhr.responseText);
+        const restaurants = json.restaurants;
+        callback(null, restaurants );
+       
+      } else { // Oops!. Got an error from server.
+        const error = (`Request failed. Returned status of ${xhr.status}`);
+        callback(error, null);
+      }
+    };
+    xhr.send();
+  }
 
 }
 
